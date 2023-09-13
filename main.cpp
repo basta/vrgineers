f1cabdb819f08fb42378d05b41c7668223449726
basta <jsembasta@gmail.com>
2023-09-13 20:10:46 +0200

    Working with 3 channel quickfix

--- main.cpp
+++ main.cpp
@@ -10,21 +10,44 @@ void error_callback(int error, const char *description) {
     fprintf(stderr, "Error: %s\n", description);
 }
 
+void expand_to_three_channels(const unsigned char * one_channel, unsigned char * out_three_channel, int one_length) {
+    for (int i = 0; i < one_length; ++i) {
+        out_three_channel[i*3] = one_channel[i];
+    }
+}
+
 
 const char *computeShaderCode = R"(
 #version 460
 
 layout(local_size_x = 1, local_size_y = 1, local_size_z = 1) in;
-layout(rgba8, binding = 0) uniform image2D imgOutput;
+layout(rgba8, binding = 0) uniform image2D monoInput;
+layout(rgba8, binding = 1) uniform image2D imgOutput;
+
+// Type definitions
+const uint RED = 1u;
+const uint GREEN = 2u;
+const uint BLUE = 3u;
+
+uint getChannel(uint x, uint y) {
+    return
+        y%2 * x%2 * BLUE // odd row odd col 1,1
+        + y%2 * (1-x%2) * GREEN // odd row, even col 1, 0
+        + (1-y%2) * (1-x%2) * RED // even row, even col, 0,0
+        + (1-y%2) * x%2 * GREEN; // 0, 1
+}
+
 
 void main() {
     vec4 value = vec4(0.0, 0.0, 0.0, 1.0);
     ivec2 texelCoord = ivec2(gl_GlobalInvocationID.xy);
-
     value.x = float(texelCoord.x)/(gl_NumWorkGroups.x);
     value.y = float(texelCoord.y)/(gl_NumWorkGroups.y);
 
-    imageStore(imgOutput, texelCoord, vec4(0.,0.,1.,1.));
+    imageStore(imgOutput, texelCoord,
+        imageLoad(monoInput, texelCoord).x + vec4(0.,0.,texelCoord.x/4056.,1.)
+    );
+
 }
 )";
 
@@ -32,19 +55,26 @@ GLuint bind_texture_from_array2D3C(const unsigned char *arr, int width, int heig
     GLuint texture;
     glGenTextures(1, &texture);
     glBindTexture(GL_TEXTURE_2D, texture);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, arr);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, arr);
     glBindImageTexture(binding, texture, 0, GL_FALSE, 0, GL_READ_WRITE, GL_RGBA8);
     return texture;
 }
+
+
 
 GLuint bind_texture_from_array2D1C(const unsigned char *arr, int width, int height, int binding) {
     GLuint texture;
     glGenTextures(1, &texture);
     glBindTexture(GL_TEXTURE_2D, texture);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    // TODO prozkoumat formáty, nejspíš je tu něco blbě
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RED, width, height, 0, GL_RED, GL_UNSIGNED_BYTE, arr);
 
-    glBindImageTexture(binding, texture, 0, GL_FALSE, 0, GL_READ_WRITE, GL_RGBA8);
+    glBindImageTexture(binding, texture, 0, GL_FALSE, 0, GL_READ_WRITE, GL_R8);
     return texture;
 }
 
@@ -112,7 +142,13 @@ int main(int argc, char **argv) {
     glLinkProgram(computeProgram);
     glUseProgram(computeProgram);
 
-    GLuint texture = bind_texture_from_array2D1C(data, width, height, 0);
+    auto outData = new unsigned char [width*height*3];
+    GLuint outTexture = bind_texture_from_array2D3C(outData, width, height, 1);
+
+    auto C3in = new unsigned char[width*height*3];
+    expand_to_three_channels(data, C3in, width*height);
+    GLuint texture = bind_texture_from_array2D3C(C3in, width, height, 0);
+
 
     glDispatchCompute(width, height, 1); // Number of work groups
 
@@ -121,7 +157,7 @@ int main(int argc, char **argv) {
 
     auto outImg = new unsigned char[width * height * 4];
     glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
-    glBindTexture(GL_TEXTURE_2D, texture);
+    glBindTexture(GL_TEXTURE_2D, outTexture);
     glGetTexImage(GL_TEXTURE_2D, 0, GL_RGBA, GL_UNSIGNED_BYTE, outImg);
     save_img("/tmp/shaderOut.png", outImg, width, height, 4);
 
